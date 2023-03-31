db7e3f0fc67fc4e8d42fb572dc9aeb3ff1946afc
estefan <29024424+steandres@users.noreply.github.com>
2023-03-30 23:37:18 -0500

    display a picture on the screen

---
+++ 5-exercise_1.c
@@ -0,0 +1,23 @@
+/*Write a program that uses printf to display the following picture on screen
+       *
+      *
+     *
+*   *
+ * *
+  *
+
+*/
+#include<stdio.h>
+
+int main(){
+
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+
+    return 0;
+
+}
